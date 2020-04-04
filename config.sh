0b85350801c53e2b59e6494212aa8bf6f5515706
wulan17 <galihgustip@gmail.com>
2020-04-04 16:00:55 +0000

    mt6765: Add workflows
    Signed-off-by: wulan17 <galihgustip@gmail.com>

---
+++ config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export TELEGRAM_TOKEN
+export TELEGRAM_CHAT
+export GITHUB_TOKEN="$GITOKEN"
+export ARCH="arm"
+export SUBARCH="arm"
+export PATH="/usr/lib/ccache:$PATH"
+export KBUILD_BUILD_USER="wulan17"
+export KBUILD_BUILD_HOST="Github"
+export LOCALVERSION="-wulan17"
+export tc_repo="https://github.com/wulan17/linaro_arm-linux-gnueabihf-7.5.git"
+export tc_name="arm-linux-gnueabihf"
+export tc_v="7.5"
+export timezone='Asia/Jakarta'
+export zip_name="kernel-""$device""-""$(env TZ='$timezone' date +%Y%m%d)"""
+export KERNEL_DIR=$(pwd)
+export KERN_IMG="$KERNEL_DIR"/out/arch/"$ARCH"/boot/zImage-dtb
+export ZIP_DIR="$KERNEL_DIR"/anykernel
+export CONFIG_DIR="$KERNEL_DIR"/arch/"$ARCH"/configs
+export CORES=$(grep -c ^processor /proc/cpuinfo)
+export THREAD="-j$CORES"
+CROSS_COMPILE="ccache "
+CROSS_COMPILE+="$KERNEL_DIR"/"$tc_name"-"$tc_v"/bin/"$tc_name-"
+export CROSS_COMPILE
+export release_repo="wulan17/kernel_build"
