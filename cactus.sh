c880c5fc6db5970048f4074c3b4a0fb44b2ad512
wulan17 <galihgustip@gmail.com>
2021-03-11 12:41:44 +0000

    mt6765: update workflows
    Signed-off-by: wulan17 <galihgustip@gmail.com>

--- cactus.sh
+++ cactus.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 sudo apt update && sudo apt install ccache python3 python3-pip
 python3 -m pip install configparser
-alias python="python3"
 # Export
 export device="cactus"
 source config.sh
 
 function sync(){
 	SYNC_START=$(date +"%s")
-	curl -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage
-	cd "$KERNEL_DIR" && wget --quiet -O "$tc_filename".tar.xz "$tc_url" > /dev/null
-	cd "$KERNEL_DIR" && tar -xJf "$tc_filename".tar.xz && mv "$tc_filename" "$tc_name"-"$tc_v" && rm "$tc_filename".tar.xz
+	curl -v -F secret="$ci_secret" -F text="Sync Started" "$ci_url"/sendMessage
+	cd "$KERNEL_DIR" && git clone "$tc_repo" -b "$tc_branch" "$tc_name"-"$tc_v"
 	cd "$KERNEL_DIR" && git clone https://github.com/wulan17/AnyKernel3.git -b "$device" anykernel
-	chmod -R a+x "$KERNEL_DIR"/"$tc_name"-"$tc_v"
 	SYNC_END=$(date +"%s")
 	SYNC_DIFF=$((SYNC_END - SYNC_START))
-	curl -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
+	curl -F secret="$ci_secret" -F text="Sync completed successfully in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds" "$ci_url"/sendMessage > /dev/null
 }
 function build(){
 	BUILD_START=$(date +"%s")
 	cd "$KERNEL_DIR"
 	export last_tag=$(git log -1 --oneline)
-	curl -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F text="Build Started" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage > /dev/null
+	curl -F secret="$ci_secret" -F text="Build Started for $device" "$ci_url"/sendMessage > /dev/null
 	make  O=out "$device"_defconfig "$THREAD" > "$KERNEL_DIR"/kernel.log
 	make "$THREAD" O=out >> "$KERNEL_DIR"/kernel.log
 	BUILD_END=$(date +"%s")
@@ -38,22 +35,14 @@ function release(){
 }
 function success(){
 	release
-	curl -v -F "chat_id=$TELEGRAM_CHAT" -F "parse_mode=html" -F "text=Build completed successfully in $((BUILD_DIFF / 60)):$((BUILD_DIFF % 60))
-Dev : ""$KBUILD_BUILD_USER""
-Product : Kernel
-Device : #""$device""
-Branch : ""$branch""
-Host : ""$KBUILD_BUILD_HOST""
-Commit : ""$last_tag""
-Compiler : ""$(${CROSS_COMPILE}gcc --version | head -n 1)""
-Date : ""$(env TZ=Asia/Jakarta date)""
-Download: <a href='https://github.com/""$release_repo""/releases/download/""$tag""/""$zip_name"".zip'>""$zip_name"".zip</a>" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage
-	
-	curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@"$KERNEL_DIR"/kernel.log https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument > /dev/null
+	curl -v -F secret="$ci_secret" -F "text=Build completed successfully in $((BUILD_DIFF / 60)):$((BUILD_DIFF % 60))
+Download: <a href='https://github.com/""$release_repo""/releases/download/""$tag""/""$zip_name"".zip'>""$zip_name"".zip</a>" "$ci_url"/sendMessage
+
+	curl -v -F secret="$ci_secret" -F document=@"$KERNEL_DIR"/kernel.log -F "$ci_url"/sendDocument > /dev/null
 	exit 0
 }
 function failed(){
-	curl -v -F "chat_id=$TELEGRAM_CHAT" -F document=@"$KERNEL_DIR"/kernel.log -F "parse_mode=html" -F "caption=Build failed in $((BUILD_DIFF / 60)):$((BUILD_DIFF % 60))" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument > /dev/null
+	curl -v -F secret="$ci_secret" -F document=@"$KERNEL_DIR"/kernel.log -F "caption=Build failed in $((BUILD_DIFF / 60)):$((BUILD_DIFF % 60))" "$ci_url"/sendDocument > /dev/null
 	exit 1
 }
 function check_build(){
--- config.sh
+++ config.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-export TELEGRAM_TOKEN
-export TELEGRAM_CHAT
+export ci_secret
+export ci_url="https://ci.wulan17.my.id"
 export GITHUB_TOKEN="$GITOKEN"
 export ARCH="arm"
 export SUBARCH="arm"
 export PATH="/usr/lib/ccache:$PATH"
 export KBUILD_BUILD_USER="wulan17"
 export KBUILD_BUILD_HOST="Github"
-#export tc_repo="https://github.com/wulan17/linaro_arm-linux-gnueabihf-7.5.git"
-export tc_url="https://developer.arm.com/-/media/Files/downloads/gnu-a/9.2-2019.12/binrel/gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf.tar.xz\?revision\=fed31ee5-2ed7-40c8-9e0e-474299a3c4ac\&la\=en\&hash\=76DAF56606E7CB66CC5B5B33D8FB90D9F24C9D20"
-export tc_filename="gcc-arm-9.2-2019.12-x86_64-arm-none-linux-gnueabihf"
+export tc_repo="https://gitlab.com/wulan17/prebuilts_gcc_linux-x86_arm_arm-none-linux-gnueabihf.git"
 export tc_name="arm-none-linux-gnueabihf"
 export tc_v="9.2"
+export tc_branch="9.2"
 export branch="$(git branch)"
 export timezone='Asia/Jakarta'
 export zip_name="kernel-""$device""-""$(env TZ='$timezone' date +%Y%m%d)"""
