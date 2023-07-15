91647d2a6944c1f9f73bd64b80e419eef6b6bf56
wulan17 <wulan17@nusantararom.org>
2023-07-15 12:21:09 +0700

    drivers: rtc: import t-alps-q0.mp1-V9.122.1 changes
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- drivers/rtc/rtc-pcf8523.c
+++ drivers/rtc/rtc-pcf8523.c
@@ -94,9 +94,8 @@ static int pcf8523_voltage_low(struct i2c_client *client)
 	return !!(value & REG_CONTROL3_BLF);
 }
 
-static int pcf8523_load_capacitance(struct i2c_client *client)
+static int pcf8523_select_capacitance(struct i2c_client *client, bool high)
 {
-	u32 load;
 	u8 value;
 	int err;
 
@@ -104,24 +103,14 @@ static int pcf8523_load_capacitance(struct i2c_client *client)
 	if (err < 0)
 		return err;
 
-	load = 12500;
-	of_property_read_u32(client->dev.of_node, "quartz-load-femtofarads",
-			     &load);
-
-	switch (load) {
-	default:
-		dev_warn(&client->dev, "Unknown quartz-load-femtofarads value: %d. Assuming 12500",
-			 load);
-		/* fall through */
-	case 12500:
-		value |= REG_CONTROL1_CAP_SEL;
-		break;
-	case 7000:
+	if (!high)
 		value &= ~REG_CONTROL1_CAP_SEL;
-		break;
-	}
+	else
+		value |= REG_CONTROL1_CAP_SEL;
 
 	err = pcf8523_write(client, REG_CONTROL1, value);
+	if (err < 0)
+		return err;
 
 	return err;
 }
@@ -318,10 +307,9 @@ static int pcf8523_probe(struct i2c_client *client,
 	if (!pcf)
 		return -ENOMEM;
 
-	err = pcf8523_load_capacitance(client);
+	err = pcf8523_select_capacitance(client, true);
 	if (err < 0)
-		dev_warn(&client->dev, "failed to set xtal load capacitance: %d",
-			 err);
+		return err;
 
 	err = pcf8523_set_pm(client, 0);
 	if (err < 0)
