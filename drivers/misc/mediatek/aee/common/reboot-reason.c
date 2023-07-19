3a2cebe3925063c8ded67ffd98c3c17159acb41c
wulan17 <wulan17@nusantararom.org>
2023-07-19 13:29:43 +0000

    drivers: misc: mediatek: aee: import Redmi 9A/C, Poco C3 changes
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- drivers/misc/mediatek/aee/common/reboot-reason.c
+++ drivers/misc/mediatek/aee/common/reboot-reason.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -32,6 +33,7 @@
 #include <mt-plat/aee.h>
 #include <mrdump.h>
 #include "aee-common.h"
+#include <linux/cpufreq.h>
 
 #define RR_PROC_NAME "reboot-reason"
 
@@ -145,6 +147,36 @@ static struct attribute_group bootinfo_attr_group = {
 	.attrs = bootinfo_attrs,
 };
 
+static int cpumaxfreq_show(struct seq_file *m, void *v)
+{
+	unsigned long maxfreq, freq;
+	int i;
+
+	maxfreq = cpufreq_quick_get_max(0);
+	for_each_possible_cpu(i) {
+		freq = cpufreq_quick_get_max(i);
+		if (freq > maxfreq)
+			maxfreq = freq;
+	}
+	/* value is used for setting cpumaxfreq */
+	maxfreq /= 10000;
+	seq_printf(m,"%lu.%02lu", maxfreq/100, maxfreq%100);
+
+	return 0;
+}
+
+static int cpumaxfreq_open(struct inode *inode, struct file *file)
+{
+	return single_open(file, &cpumaxfreq_show, NULL);
+}
+
+static const struct file_operations proc_cpumaxfreq_operations = {
+	.open       = cpumaxfreq_open,
+	.read       = seq_read,
+	.llseek     = seq_lseek,
+	.release    = seq_release,
+};
+
 int ksysfs_bootinfo_init(void)
 {
 	int error;
@@ -156,6 +188,7 @@ int ksysfs_bootinfo_init(void)
 	error = sysfs_create_group(bootinfo_kobj, &bootinfo_attr_group);
 	if (error)
 		kobject_put(bootinfo_kobj);
+	proc_create("cpumaxfreq", 0, NULL, &proc_cpumaxfreq_operations);
 
 	return error;
 }
